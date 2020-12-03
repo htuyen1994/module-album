f5b8098a17d30eaba3fea3e0b84122fb4e7b3e0b
Nhat <dlnhat98@gmail.com>
2020-12-03 14:00:10 +0700

    Thiết kế khung các chức năng của module

---
+++ modules/album/admin.menu.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC <contact@vinades.vn>
+ * @Copyright (C) 2020 VINADES.,JSC. All rights reserved
+ * @License: Not free read more http://nukeviet.vn/vi/store/modules/nvtools/
+ * @Createdate Sat, 31 Oct 2020 02:20:33 GMT
+ */
+
+if (!defined('NV_ADMIN')) {
+    die('Stop!!!');
+}
+
+$submenu['add_album'] = $lang_module['add_album'];
+$submenu['add_image'] = $lang_module['add_image'];
+$submenu['list'] = $lang_module['list'];
