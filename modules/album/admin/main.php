6acaa7e4465f205a1202431f77d6656d228c8505
Nhat <dlnhat98@gmail.com>
2020-12-03 14:58:17 +0700

    Danh sách album bao gồm module sửa, xóa album

--- modules/album/admin/main.php
+++ modules/album/admin/main.php
@@ -12,11 +12,71 @@ if (!defined('NV_IS_FILE_ADMIN')) {
     die('Stop!!!');
 }
 
+if ($nv_Request->isset_request("action", "post,get")) {
+    $id = $nv_Request->get_int('id', "post, get", 0);
+    $checksess = $nv_Request->get_title('checksess', "post, get", '');
+    if ($id > 0 and $checksess == md5($id . NV_CHECK_SESSION)) {
+        $db->query("DELETE FROM `nv4_album` WHERE id=" . $id);
+    }
+}
+
+if ($nv_Request->isset_request("change_active", "post,get")) {
+    $id = $nv_Request->get_int('id', "post, get", 0);
+    $sql = "SELECT id, active FROM nv4_album WHERE id = " . $id;
+    $result = $db->query($sql);
+    if ($row = $result->fetch()) {
+        $active = $row['active'] == 1 ? 0 : 1;
+        $exe = $db->query("UPDATE `nv4_album` SET active =" . $active . " WHERE id=" . $id);
+        if ($exe) {
+            die("OK");
+        }
+    }
+    die("ERR");
+}
+
+if ($nv_Request->isset_request("change_weight", "post,get")) {
+    $id = $nv_Request->get_int('id', "post, get", 0);
+    $new_weight = $nv_Request->get_int('new_weight', "post, get", 0);
+    if ($id > 0 and $new_weight > 0) {
+        $sql = "SELECT id, weight FROM nv4_album WHERE id !=  " . $id;
+        $result = $db->query($sql);
+        $weight = 0;
+        while ($row = $result->fetch()) {
+            ++$weight;
+            if ($weight == $new_weight) {
+                ++$weight;
+            }
+            $exe = $db->query("UPDATE `nv4_album` SET weight =" . ($weight) . " WHERE id=" . $row['id']);
+        }
+        $exe = $db->query("UPDATE `nv4_album` SET weight =" . $new_weight . " WHERE id=" . $id);
+        if ($exe) {
+            die('OK');
+        }
+    }
+    die('ERR');
+}
+
 $page_title = $lang_module['main'];
 
-//------------------------------
-// Viết code xử lý chung vào đây
-//------------------------------
+$perpage = 5;
+$page = $nv_Request->get_int('page', " get", 1);
+
+$db->sqlreset()
+->select('COUNT(*)')
+->from('nv4_album');
+$sql = $db->sql();
+$total = $db->query($sql)->fetchColumn();
+
+$db->select('*')
+->order("weight ASC")
+->limit($perpage)
+->offset(($page - 1) * $perpage);
+
+$sql = $db->sql();
+$result = $db->query($sql);
+while ($row = $result->fetch()) {
+    $array_row[$row['id']] = $row;
+}
 
 $xtpl = new XTemplate('main.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file);
 $xtpl->assign('LANG', $lang_module);
@@ -28,9 +88,32 @@ $xtpl->assign('NV_OP_VARIABLE', NV_OP_VARIABLE);
 $xtpl->assign('MODULE_NAME', $module_name);
 $xtpl->assign('OP', $op);
 
-//-------------------------------
-// Viết code xuất ra site vào đây
-//-------------------------------
+if (!empty($array_row)) {
+    $i = ($page - 1) * $perpage;
+    foreach ($array_row as $row) {
+        $row['stt'] = $i + 1;
+        for ($j = 1; $j <= $total; $j++) {
+            $xtpl->assign('J', $j);
+            $xtpl->assign('J_SELECT', $j == $row['weight'] ? 'selected="selected"' : '');
+            $xtpl->parse('main.loop.weight');
+        }
+        $row['url_edit'] = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE
+        . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=add_album&amp;id=' . $row['id'];
+        $row['url_delete'] = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name
+        . '&amp;' . NV_OP_VARIABLE . '=main&amp;id=' . $row['id'] . '&action=delete&checksess=' . md5($row['id'] . NV_CHECK_SESSION);
+        $row['active'] = $row['active'] == 1 ? 'checked="checked"' : '';
+        if(!empty('image')){
+            $row['image'] = NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_name . '/' . $row['image'];
+        }
+        $xtpl->assign('ROW', $row);
+        $xtpl->parse('main.loop');
+        $i++;
+    }
+}
+
+$base_url = NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=' . $module_name . '&amp;' . NV_OP_VARIABLE . '=list';
+$generate_page = nv_generate_page($base_url, $total, $perpage, $page);
+$xtpl->assign('GENERATE_PAGE', $generate_page);
 
 $xtpl->parse('main');
 $contents = $xtpl->text('main');
--- themes/admin_default/modules/album/main.tpl
+++ themes/admin_default/modules/album/main.tpl
@@ -1,5 +1,83 @@
 <!-- BEGIN: main -->
-<form action="{NV_BASE_ADMINURL}index.php?{NV_LANG_VARIABLE}={NV_LANG_DATA}&amp;{NV_NAME_VARIABLE}={MODULE_NAME}&amp;{NV_OP_VARIABLE}={OP}" method="post">
-    <div class="text-center"><input class="btn btn-primary" name="submit" type="submit" value="{LANG.save}" /></div>
-</form>
+<div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover">
+        <thead>
+            <tr class="text-center">
+                <th class="text-nowrap">Số thứ tự</th>
+                <th class="text-nowrap">Tên album</th>
+                <th class="text-nowrap">Ảnh</th>
+                <th class="text-nowrap">Mô tả</th>
+                <th class="text-nowrap">ID người đăng</th>
+                <th class="text-nowrap">Thời gian đăng</th>              
+                <th class="text-nowrap">Kích hoạt</th>
+                <th class="text-center text-nowrap">Chức năng</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- BEGIN: loop -->
+            <tr>
+                <td class="text-center">
+                     <select name="weight" class="form-control weight_{ROW.id}"  onchange="nv_change_weight({ROW.id})">
+                     <!-- BEGIN: weight -->
+                        <option value="{J}" {J_SELECT}>{J}</option>
+                        <!-- END: weight -->
+                     </select>
+                </td>
+                <td>{ROW.name}</td>
+                <td>
+                	<img src="{ROW.image}" width="100px" height="100px">
+                </td>
+                <td>{ROW.description}</td>
+                <td></td>
+                <td></td>
+                <td class="text-center">
+                    <input type="checkbox" name="active" {ROW.active} onchange="nv_change_active({ROW.id})">
+                </td>
+                <td class="text-center text-nowrap">
+                <a href="{ROW.url_edit}" class="btn btn-default btn-sm"><i class="fa fa-edit"></i> Sửa</a>
+                <a href="{ROW.url_delete}" class="btn btn-danger btn-sm delete"><i class="fa fa-trash-o"></i> Xóa</a></td>
+            </tr>
+            <!-- END: loop -->
+        </tbody>
+    </table>
+    {GENERATE_PAGE}
+</div>
+<script>
+function nv_change_weight(id) {
+    var new_weight = $('.weight_'+ id).val();
+    $.ajax({
+        url : script_name + '?' + nv_name_variable + '=' + nv_module_name
+                + '&' + nv_fc_variable
+                + '=list_product&change_weight=1&id=' + id + '&new_weight='+new_weight,
+        success : function(result) {
+            if (result != 'ERR') {
+                location.reload();
+            }
+        }
+    });
+}
+function nv_change_active(id) {
+    $.ajax({
+        url : script_name + '?' + nv_name_variable + '=' + nv_module_name
+                + '&' + nv_fc_variable
+                + '=list_product&change_active=1&id=' + id,
+        success : function(result) {
+            if (result == 'ERR') {
+                alert('Lỗi k xác định');
+                location.reload();
+            }
+        }
+    });
+}
+$(document).ready(function (){
+    $('.delete').click(function () {
+        if (confirm("bạn có muốn xóa!")) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+});
+</script>
+
 <!-- END: main -->
